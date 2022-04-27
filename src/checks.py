1bb810faa8df3141512cd9b5c2e026c50c88db02
Matan Perlmutter <matan@deepchecks.com>
2022-04-27 13:39:06 +0300

    Moved some logic out of streamlit_app.py

--- src/checks.py
+++ src/checks.py
@@ -1,12 +1,22 @@
+import io
+import json
+from typing import Sequence
+
+import streamlit as st
 from deepchecks.tabular.checks import SimpleModelComparison
 from deepchecks.tabular.checks.distribution import TrainTestFeatureDrift, TrainTestLabelDrift
 from deepchecks.tabular.checks.integrity import StringMismatch, DataDuplicates
 from deepchecks.tabular.checks.performance import SegmentPerformance
+import streamlit.components.v1 as components
 
 import run_train_test_feature_drift, run_train_test_label_drift, run_string_mismatch, run_data_duplicates, \
     run_segment_performance, run_simple_model_comparison
+from datasets import get_dataset_options
 
-__all__ = ['get_checks_options']
+
+__all__ = ['show_checks_page']
+
+from encoder import AppEncoder
 
 
 def get_checks_options():
@@ -19,3 +29,61 @@ def get_checks_options():
         SimpleModelComparison: run_simple_model_comparison.run
     }
 
+
+def show_checks_page():
+    TEMPLATE_WRAPPER = """
+    <div style="height:{height}px;overflow-y:auto;position:relative;">
+        {body}
+    </div>
+    """
+
+    col1, col2 = st.columns(2)
+
+    with st.spinner('Loading datasets...'):
+        datasets = get_dataset_options()
+
+    checks = get_checks_options()
+    name_to_class = {check_key.name(): check_key for check_key in checks.keys()}
+
+    # First select a dataset and check
+    dataset_name = st.sidebar.selectbox('Select a dataset', datasets.keys())
+    check_name = st.sidebar.selectbox('Select a check', name_to_class.keys())
+
+    dataset = datasets[dataset_name]
+    check_class = name_to_class[check_name]
+    check_run = checks[check_class]
+
+    # Run the check
+    with col1:
+        with st.spinner('Running check'):
+            check_result, snippet = check_run(dataset)
+            if isinstance(check_result, str):
+                with col1:
+                    st.error(check_result)
+                result_html = None
+                result_value = None
+            else:
+                string_io = io.StringIO()
+                check_result.save_as_html(string_io)
+                result_html = string_io.getvalue()
+                result_value = check_result.value
+
+        st.code(snippet, language='python')
+
+        if result_value is not None:
+            with st.expander('print(result.value)'):
+                # If the result value is simple type (e.g. int, float, str) it can't be displayed as json
+                if isinstance(result_value, (dict, Sequence)):
+                    result_value = json.dumps(result_value, indent=4, sort_keys=False, cls=AppEncoder)
+                    st.json(result_value)
+                else:
+                    st.code(str(result_value), language='python')
+
+        if result_html:
+            height_px = 800
+            html = TEMPLATE_WRAPPER.format(body=result_html, height=height_px)
+            components.html(html, height=height_px)
+
+    with col2:
+        st.subheader(f'Check {check_class.__name__} docstring')
+        st.text(check_class.__doc__)
--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,82 +1,20 @@
-import io
-import json
 from pathlib import Path
-from typing import Sequence
-
-from PIL import Image
 
 import streamlit as st
-import streamlit.components.v1 as components
-
-from analytics import inject_ga
-from datasets import get_dataset_options
-from checks import get_checks_options
-from encoder import AppEncoder
+from PIL import Image
 from dotenv import load_dotenv
 
+from analytics import inject_ga
+from checks import show_checks_page
 
 # Add google analytics
 load_dotenv()
 inject_ga()
-
-TEMPLATE_WRAPPER = """
-<div style="height:{height}px;overflow-y:auto;position:relative;">
-    {body}
-</div>
-"""
 
 icon = Image.open(Path(__file__).parent.parent / 'resources' / 'favicon.ico')
 logo = open(Path(__file__).parent.parent / 'resources' / 'deepchecks_logo.svg').read()
 
 st.set_page_config(page_title='Deepchecks Demo', page_icon=icon, layout='wide')
 st.sidebar.image(logo, use_column_width=True)
-col1, col2 = st.columns(2)
 
-with st.spinner('Loading datasets...'):
-    datasets = get_dataset_options()
-
-checks = get_checks_options()
-name_to_class = {check_key.name(): check_key for check_key in checks.keys()}
-
-# First select a dataset and check
-dataset_name = st.sidebar.selectbox('Select a dataset', datasets.keys())
-check_name = st.sidebar.selectbox('Select a check', name_to_class.keys())
-
-dataset = datasets[dataset_name]
-check_class = name_to_class[check_name]
-check_run = checks[check_class]
-
-# Run the check
-with col1:
-    with st.spinner('Running check'):
-        check_result, snippet = check_run(dataset)
-        if isinstance(check_result, str):
-            with col1:
-                st.error(check_result)
-            result_html = None
-            result_value = None
-        else:
-            string_io = io.StringIO()
-            check_result.save_as_html(string_io)
-            result_html = string_io.getvalue()
-            result_value = check_result.value
-
-    st.code(snippet, language='python')
-
-    if result_value is not None:
-        with st.expander('print(result.value)'):
-            # If the result value is simple type (e.g. int, float, str) it can't be displayed as json
-            if isinstance(result_value, (dict, Sequence)):
-                result_value = json.dumps(result_value, indent=4, sort_keys=False, cls=AppEncoder)
-                st.json(result_value)
-            else:
-                st.code(str(result_value), language='python')
-
-    if result_html:
-        height_px = 800
-        html = TEMPLATE_WRAPPER.format(body=result_html, height=height_px)
-        components.html(html, height=height_px)
-
-with col2:
-    st.subheader(f'Check {check_class.__name__} docstring')
-    st.text(check_class.__doc__)
+show_checks_page()
