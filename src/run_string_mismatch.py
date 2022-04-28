e270885d0c9499677d8d994a41ed8ab369ce948e
Matan Perlmutter <matan@deepchecks.com>
2022-04-28 17:17:40 +0300

    Update string variants to be more realistic

--- src/run_string_mismatch.py
+++ src/run_string_mismatch.py
@@ -36,22 +36,26 @@ def run(dataset_option: DatasetOption, check_param_col, manipulate_col):
 def insert_variants(column: pd.Series):
     column = column.to_numpy()
     value = np.random.choice(column)
+    variants = random_variants(value)
     possible_indices = np.where(column != value)
-    indices_to_replace = np.random.choice(possible_indices[0], size=4, replace=False)
+    indices_to_replace = np.random.choice(possible_indices[0], size=10, replace=False)
     for index in indices_to_replace:
-        column[index] = random_variant(value)
+        column[index] = random.choice(variants)
     return column
 
 
-def random_variant(value):
-    special_chars = '!@#$%^&*()_+{}[]|\:;"<>?,./'
-    value = str(value)
-    random_index = random.randint(0, len(value) - 1)
-    val_in_index = value[random_index]
-    if val_in_index.isalpha():
-        changed_char = val_in_index.upper() if val_in_index.islower() else val_in_index.lower()
-        value = value[:random_index] + changed_char + value[random_index + 1:]
-    else:
-        value = value[:random_index] + random.choice(special_chars) + value[random_index:]
+def flip_case(value):
+    return value.upper() if value.islower() else value.lower()
 
-    return value
+
+def random_variants(value):
+    functions = [
+        lambda x: flip_case(x[0]) + x[1:],  # Switch upper lower first letter
+        lambda x: x.replace(' ', '-'),  # Replace space with -
+        lambda x: x + '.',  # Add . at the end
+        lambda x: x.upper(),  # Upper case all letters
+        lambda x: x.lower(),  # Lower case all letters
+        lambda x: ' ' + x,  # Add space at the beginning
+    ]
+
+    return [f(value) for f in functions if f(value) != value]
