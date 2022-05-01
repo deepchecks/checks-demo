89516612c83419ae5f50961d922dd4f80273c905
Matan Perlmutter <matan@deepchecks.com>
2022-05-01 15:37:07 +0300

    Add link to logo

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -15,8 +15,9 @@ inject_meta_tags()
 
 icon = Image.open(Path(__file__).parent.parent / 'resources' / 'favicon.ico')
 logo = open(Path(__file__).parent.parent / 'resources' / 'deepchecks_logo.svg').read()
+logo_with_link = f'<a href="https://deepchecks.com" target="_blank">{logo}</a>'
 
 st.set_page_config(page_title='Deepchecks Checks Demo', page_icon=icon, layout='wide')
-st.sidebar.image(logo, use_column_width=True)
+st.sidebar.markdown(logo_with_link, unsafe_allow_html=True)
 
 show_checks_page()
